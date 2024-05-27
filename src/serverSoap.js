c13ed270e08581582b4a76139054368ee73c6db9
Tristan-2i <78900902+Tristan-2i@users.noreply.github.com>
2024-05-27 11:24:47 +0200

    Fin TP

--- src/serverSoap.js
+++ src/serverSoap.js
@@ -21,6 +21,31 @@ const service = {
                 return {
                     res: n
                 };
+            },
+
+            dividir: function (args) {
+                console.log('dividir called');
+                console.log(args);
+                if (parseInt(args.b) === 0) {
+                    throw {
+                        Fault: {
+                            Code: {
+                                Value: 'soap:Sender',
+                                Subcode: {
+                                    value: 'rpc:BadArguments'
+                                }
+                            },
+                            Reason: {
+                                Text: 'Division by zero'
+                            },
+                            statusCode: 500
+                        }
+                    };
+                }
+                var n = parseInt(args.a) / parseInt(args.b);
+                return {
+                    res: n
+                };
             }
         }
     }
