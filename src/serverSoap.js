a9a6cad631d7886ce4d9853dbb49b346cf8c8575
Thomas Gourdon <tgourdon59@gmail.com>
2024-05-27 11:35:50 +0200

    TP SOAP Fini

--- src/serverSoap.js
+++ src/serverSoap.js
@@ -21,12 +21,35 @@ const service = {
                 return {
                     res: n
                 };
+                
+            },
+            divar: function (args, callback) {
+                console.log('divar called');
+                console.log(args);
+                if (isNaN(args.a) || isNaN(args.b)) {
+                    return callback({
+                        Fault: {
+                            faultcode: 'Client',
+                            faultstring: 'Invalid input: a and b must be integers'
+                        }
+                    });
+                }
+                if (parseInt(args.b) === 0) {
+                    return callback({
+                        Fault: {
+                            faultcode: '500',
+                            faultstring: 'Division by zero error'
+                        }
+                    });
+                }
+                var n = parseInt(args.a) / parseInt(args.b);
+                callback(null, { divres: n });
             }
         }
     }
 };  
 
-var xml = fs.readFileSync('./src/wscalc1.wsdl', 'utf8');
+var xml = fs.readFileSync('./Sources/wscalc1.wsdl', 'utf8');
 
 const server = http.createServer(function (request, response) {
     response.end('404: Not Found: ' + request.url);
