7ba625ddf3bb8ca2dd20ad8114dadc8d0fc35392
Paul-Colin <paul.colin@ig2i.centralelille.fr>
2024-05-27 11:14:28 +0200

    Premier push

--- src/serverSoap.js
+++ src/serverSoap.js
@@ -21,6 +21,33 @@ const service = {
                 return {
                     res: n
                 };
+            },
+            divar: function (args) {
+                console.log('divisar called');
+                console.log(args);
+                if(args.a > 0 && args.b > 0 && args != null){
+                    var n = parseInt(args.a) / parseInt(args.b);
+                    return {
+                     res: n
+                };
+                }
+                else{
+                    // return {
+                    //     res: "On ne peut pas diviser par 0 ou des valeurs inférieures à 0" 
+                    // };
+                    /*
+                    Erreur voulu pour tester le code
+                    Code : 500
+                    Reason : b cannot be 0
+                    */
+                    throw  {
+                        Fault: {
+                            faultcode: "500",
+                            faultstring: "b cannot be 0",
+                            detail: "On ne peut pas diviser par 0 ou des valeurs inférieures à 0"
+                        }
+                    }
+                }
             }
         }
     }
