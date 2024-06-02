57e83ae4f28d070315715ccf934af4f62a5512ba
Antoine <antoine.mol@ig2i.centralelille.fr>
2024-06-02 22:22:39 +0200

    TPP SOAP fini

--- src/serverSoap.js
+++ src/serverSoap.js
@@ -3,35 +3,50 @@ const http = require('http');
 const soap = require('soap');
 
 const service = {
-    ws: {
-        calc: {
-            sumar: function (args) {
-                console.log('sumar called');
-                console.log(args);
-                var n = parseInt(args.a) + parseInt(args.b);
-                return {
-                    res: n
-                };
-            },
+	ws: {
+		calc: {
+			sumar: function (args) {
+				console.log('sumar called');
+				console.log(args);
+				var n = parseInt(args.a) + parseInt(args.b);
+				return {
+					res: n
+				};
+			},
 
-            multiplicar: function (args) {
-                console.log('multiplicar called');
-                console.log(args);
-                var n = parseInt(args.a) * parseInt(args.b);
-                return {
-                    res: n
-                };
-            },
-            divar: function (args) {
-                console.log('divar called');
-                console.log(args);
-                var n = parseInt(args.a) / parseInt(args.b);
-                return {
-                    res: n
-                };
-            } 
-        }
-    }
+			multiplicar: function (args) {
+				console.log('multiplicar called');
+				console.log(args);
+				var n = parseInt(args.a) * parseInt(args.b);
+				return {
+					res: n
+				};
+			},
+            
+			divar: function (args) {
+				console.log('divar called');
+				console.log(args);
+				if(parseInt(args.b)==0){
+				throw {
+					Fault: {
+						Code: {
+							Value: 'soap:Sender',
+							Subcode: { value: 'rpc:BadArguments' }
+						},
+						Reason: { Text: 'b cannot be 0' },
+						statusCode: 500
+					}
+				};
+				};
+				var n = parseInt(args.a) / parseInt(args.b);
+				
+				return {
+					res: n
+				};
+			 
+			} 
+		}
+	}
 };  
 
 var xml = fs.readFileSync('./src/wscalc1.wsdl', 'utf8');
