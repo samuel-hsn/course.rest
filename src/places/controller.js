406b1b07c5f0284e48540aa2e265058ab97e64ee
Antoine <antoine.mol@ig2i.centralelille.fr>
2024-06-10 12:18:08 +0200

    Début 13. JSON PATCH

--- src/places/controller.js
+++ src/places/controller.js
@@ -1,9 +1,13 @@
+const jsonpatch = require('fast-json-patch');
+
 class Places {
   constructor(data) {
     this.data = data;
   }
   configure(app) {
     const data = this.data;
+
+    //Validator JSON qui vérifie que les données envoyées dans les requêtes POST sont bien au format attendu
     var Validator = require('jsonschema').Validator;
     var v = new Validator();
     var placeSchema = {
@@ -25,6 +29,7 @@ class Places {
         "required": ["author", "review", "name"]
       };
 
+    //Requête GET qui renvoie la liste des places
     app.get("/api/places", async (request, response) => {
       const places = await data.getPlacesAsync();
       if (places !== undefined) {
@@ -34,6 +39,18 @@ class Places {
       response.status(404).json({key: "entity.not.found"});
     });
 
+    //Requête GET qui renvoie une place en fonction de son id
+    app.get("/api/places/:id", async (request, response) => {
+      let id = request.params.id;
+      const place = await data.getPlaceAsync(id);
+      if (place !== undefined) {
+        response.status(200).json(place);
+        return;
+      }
+      response.status(404).json({key: "entity.not.found"});
+    });
+
+    //Requête POST qui permet d'ajouter une place
     app.post("/api/places", async (request, response) => {
       const place = request.body;
       var result = v.validate(place, placeSchema);
@@ -45,16 +62,80 @@ class Places {
       response.status(201).header("Location", `/api/places/${id}`).send();
     });
 
-    app.get("/api/places/:id", async (request, response) => {
+    //Requête DELETE qui permet de supprimer une place en fonction de son id
+    app.delete("/api/places/:id", async (request, response) => {
       let id = request.params.id;
-      const place = await data.getPlaceAsync(id);
-      if (place !== undefined) {
-        response.status(200).json(place);
+      const res = await data.deletePlaceAsync(id);
+      if (res) {
+        response.status(200).json({key: "entity.deleted"});
         return;
       }
       response.status(404).json({key: "entity.not.found"});
+    }); 
+
+    //Requête PUT qui permet de modifier une place en fonction de son id
+    app.put("/api/places/:id", async (request, response) => {
+      let id = request.params.id;
+      
+      //place inexistant
+      const places = await data.getPlaceAsync(id);
+      if(places === undefined){
+        response.status(404).json({key: "entity.not.found"});
+        return;
+      }
+      const place = request.body;
+      place.id = id;
+      var result = v.validate(place, placeSchema);
+      //données invalides
+      if (result.errors.length > 0) {
+        response.status(400).json({key: "invalid.data"});
+        return;
+      }
+      const res = await data.savePlaceAsync(place);
+      response.status(200).json({key: "entity.updated"});
     });
-    
+
+    //Requête PATCH qui permet de modifier PARTEILLEMENT une place en fonction de son id
+    app.patch("/api/places/:id", async (request, response) => {
+      let id = request.params.id;
+      
+      //place inexistant
+      const placeOld = await data.getPlaceAsync(id);
+      if(placeOld === undefined){
+        response.status(404).json({key: "entity.not.found"});
+        return;
+      }
+
+      //use either JSON Patch or Object.assign
+      var key;
+      var place;
+      if(request.is("application/json-patch+json")==="application/json-patch+json"){
+      //   key="entity.updated.jsonpatch";
+        
+      //   var patch = request.body;
+      //   place = jsonpatch.applyPatch(placeOld, patch).newDocument;
+
+      } 
+      else {
+        key="entity.updated";
+        // if (Array.isArray(request.body)){
+        //   response.status(400).json({key: "invalid.data"});
+        //   return;
+        // }
+        place = Object.assign(placeOld,request.body);
+        
+      }
+      //place.id = id;
+
+      var result = v.validate(place, placeSchema);
+      //données invalides
+      if (result.errors.length > 0) {
+        response.status(400).json({key: "invalid.data"});
+        return;
+      }
+      const res = await data.savePlaceAsync(place);
+      response.status(200).json({key: key});
+    });
   }
 }
 module.exports = Places;
--- src/places/controller.spec.js
+++ src/places/controller.spec.js
@@ -27,7 +27,7 @@ describe("Places/controller", () => {
       });
   });
 
-  //TODO Ajouter ici le test qui vérifie le nombre de place remonté par l'api
+  //Test qui vérifie le nombre de place remonté par l'api
 
     it("GET /api/places should respond a http 200 OK", () => {
         const app = new App(new Place(new PlaceData())).app;
@@ -40,6 +40,9 @@ describe("Places/controller", () => {
             });
     });
 
+    /** FIN GET **/
+
+    /************* POST REQUESTS ********************************************/ 
     it('POST /api/places should respond a http 201 OK with no image', () => {
         var newPlace = {
             name: 'Londre',
@@ -109,4 +112,403 @@ describe("Places/controller", () => {
             .expect(400);
 
     });
+
+    /************************************* REQUEST DELETE **************************/
+    // it('DELETE /api/places/2 should respond a http 200 OK', () => {
+
+    //     const app = new App(new Place(new PlaceData())).app;
+    //     return request(app)
+    //     .delete("/api/places/2")
+    //     .expect("Content-Type", /json/)
+    //     .expect(200)
+
+    // });
+    // it('DELETE /api/places/999 should respond a http 400 KO', () => {
+
+    //     const app = new App(new Place(new PlaceData())).app;
+    //     return request(app)
+    //     .delete("/api/places/999")
+    //     .expect("Content-Type", /json/)
+    //     .expect(404)
+    // });
+
+
+    /**************************PUT REQUESTS***********************************/
+    it('PUT /api/places/2 should respond a http 200 OK with no image', () => {
+        var newPlace = {
+            name: 'Londre',
+            author: 'Patrick',
+            review: 2
+        };
+        const app = new App(new Place(new PlaceData())).app;
+        return request(app)
+            .put('/api/places/2')
+            .send(newPlace)
+            .expect(200);
+    });
+
+    it('PUT /api/places/2 should respond a http 200 OK with an image', () => {
+
+        var newPlace = {
+            name: 'Londre',
+            author: 'Patrick',
+            review: 2,
+            image: {
+                url: 'https://www.bworld.fr/api/file/get/c27e39ee-7ba9-46f8-aa7c-9e334c72a96c/d9d0634b-b1a0-42bd-843d-d3bc3cf7d842/ImageThumb/bworld-2016-v3.png',
+                title: 'bworld place'
+            }
+        };
+        const app = new App(new Place(new PlaceData())).app;
+        return request(app)
+            .put('/api/places/2')
+            .send(newPlace)
+            .expect(200);
+
+    });
+
+    it('PUT /api/places/2 should respond a http 400 KO', () => {
+
+        var newPlace = {
+            name: '',
+            author: 'Pat',
+            review: 2
+        };
+        const app = new App(new Place(new PlaceData())).app;
+        return request(app)
+            .put('/api/places/2')
+            .send(newPlace)
+            .expect(400);
+
+    });
+
+    it('PUT /api/places/2 should respond a http 400 KO', () => {
+
+        const app = new App(new Place(new PlaceData())).app;
+        var newPlace = {
+            name: 'Londre &',
+            author: 'Patrickmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm',
+            review: 2,
+            image: {
+                url: 'https://www.bworld.fr/api/file/get/c27e39ee-7ba9-46f8-aa7c-9e334c72a96c/d9d0634b-b1a0-42bd-843d-d3bc3cf7d842/ImageThumb/bworld-2016-v3.png',
+                title: ''
+            }
+        };
+        return request(app)
+            .put('/api/places/2')
+            .send(newPlace)
+            .expect(400);
+
+    });
+
+    it('PUT /api/places/999 should respond a http 404 KO with an image', () => {
+
+        var newPlace = {
+            name: 'Londre',
+            author: 'Patrick',
+            review: 2,
+            image: {
+                url: 'https://www.bworld.fr/api/file/get/c27e39ee-7ba9-46f8-aa7c-9e334c72a96c/d9d0634b-b1a0-42bd-843d-d3bc3cf7d842/ImageThumb/bworld-2016-v3.png',
+                title: 'bworld place'
+            }
+        };
+        const app = new App(new Place(new PlaceData())).app;
+        return request(app)
+            .put('/api/places/999')
+            .send(newPlace)
+            .expect(404);
+
+    });
+
+    it('PUT /api/places/999 should respond a http 404 KO', () => {
+
+        var newPlace = {
+            name: '',
+            author: 'Pat',
+            review: 2
+        };
+        const app = new App(new Place(new PlaceData())).app;
+        return request(app)
+            .put('/api/places/999')
+            .send(newPlace)
+            .expect(404);
+
+    });
+
+    /**************************PUT REQUESTS***********************************/
+    it('PUT /api/places/2 should respond a http 200 OK with no image', () => {
+        var newPlace = {
+            name: 'Londre',
+            author: 'Patrick',
+            review: 2
+        };
+        const app = new App(new Place(new PlaceData())).app;
+        return request(app)
+            .put('/api/places/2')
+            .send(newPlace)
+            .expect(200);
+    });
+
+    it('PUT /api/places/2 should respond a http 200 OK with an image', () => {
+
+        var newPlace = {
+            name: 'Londre',
+            author: 'Patrick',
+            review: 2,
+            image: {
+                url: 'https://www.bworld.fr/api/file/get/c27e39ee-7ba9-46f8-aa7c-9e334c72a96c/d9d0634b-b1a0-42bd-843d-d3bc3cf7d842/ImageThumb/bworld-2016-v3.png',
+                title: 'bworld place'
+            }
+        };
+        const app = new App(new Place(new PlaceData())).app;
+        return request(app)
+            .put('/api/places/2')
+            .send(newPlace)
+            .expect(200);
+    });
+
+    it('PUT /api/places/2 should respond a http 400 KO', () => {
+
+        var newPlace = {
+            name: '',
+            author: 'Pat',
+            review: 2
+        };
+        const app = new App(new Place(new PlaceData())).app;
+        return request(app)
+            .put('/api/places/2')
+            .send(newPlace)
+            .expect(400);
+    });
+
+    it('PUT /api/places/2 should respond a http 400 KO', () => {
+
+        const app = new App(new Place(new PlaceData())).app;
+        var newPlace = {
+            name: 'Londre &',
+            author: 'Patrickmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm',
+            review: 2,
+            image: {
+                url: 'https://www.bworld.fr/api/file/get/c27e39ee-7ba9-46f8-aa7c-9e334c72a96c/d9d0634b-b1a0-42bd-843d-d3bc3cf7d842/ImageThumb/bworld-2016-v3.png',
+                title: ''
+            }
+        };
+        return request(app)
+            .put('/api/places/2')
+            .send(newPlace)
+            .expect(400);
+    });
+
+    it('PUT /api/places/999 should respond a http 404 KO with an image', () => {
+
+        var newPlace = {
+            name: 'Londre',
+            author: 'Patrick',
+            review: 2,
+            image: {
+                url: 'https://www.bworld.fr/api/file/get/c27e39ee-7ba9-46f8-aa7c-9e334c72a96c/d9d0634b-b1a0-42bd-843d-d3bc3cf7d842/ImageThumb/bworld-2016-v3.png',
+                title: 'bworld place'
+            }
+        };
+        const app = new App(new Place(new PlaceData())).app;
+        return request(app)
+            .put('/api/places/999')
+            .send(newPlace)
+            .expect(404);
+    });
+
+    it('PUT /api/places/999 should respond a http 404 KO', () => {
+
+        var newPlace = {
+            name: '',
+            author: 'Pat',
+            review: 2
+        };
+        const app = new App(new Place(new PlaceData())).app;
+        return request(app)
+            .put('/api/places/999')
+            .send(newPlace)
+            .expect(404);
+    });
+
+        /**************************PATCH REQUESTS***********************************/
+        it('PATCH /api/places/2 should respond a http 200 OK with no image', () => {
+            var newPlace = {
+                name: 'Londre',
+                author: 'Patrick',
+                review: 2
+            };
+            const app = new App(new Place(new PlaceData())).app;
+            return request(app)
+                .patch('/api/places/2')
+                .send(newPlace)
+                .expect(200);
+        });
+    
+        it('PATCH /api/places/2 should respond a http 200 OK with an image', () => {
+    
+            var newPlace = {
+                name: 'Londre',
+                author: 'Patrick',
+                review: 2,
+                image: {
+                    url: 'https://www.bworld.fr/api/file/get/c27e39ee-7ba9-46f8-aa7c-9e334c72a96c/d9d0634b-b1a0-42bd-843d-d3bc3cf7d842/ImageThumb/bworld-2016-v3.png',
+                    title: 'bworld place'
+                }
+            };
+            const app = new App(new Place(new PlaceData())).app;
+            return request(app)
+                .patch('/api/places/2')
+                .send(newPlace)
+                .expect(200);
+        });
+    
+        it('PATCH /api/places/2 should respond a http 400 KO', () => {
+    
+            var newPlace = {
+                name: '',
+                author: 'Pat',
+                review: 2
+            };
+            const app = new App(new Place(new PlaceData())).app;
+            return request(app)
+                .patch('/api/places/2')
+                .send(newPlace)
+                .expect(400);
+        });
+    
+        it('PATCH /api/places/2 should respond a http 400 KO', () => {
+    
+            const app = new App(new Place(new PlaceData())).app;
+            var newPlace = {
+                name: 'Londre &',
+                author: 'Patrickmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm',
+                review: 2,
+                image: {
+                    url: 'https://www.bworld.fr/api/file/get/c27e39ee-7ba9-46f8-aa7c-9e334c72a96c/d9d0634b-b1a0-42bd-843d-d3bc3cf7d842/ImageThumb/bworld-2016-v3.png',
+                    title: ''
+                }
+            };
+            return request(app)
+                .patch('/api/places/2')
+                .send(newPlace)
+                .expect(400);
+        });
+    
+        it('PATCH /api/places/999 should respond a http 404 KO with an image', () => {
+    
+            var newPlace = {
+                name: 'Londre',
+                author: 'Patrick',
+                review: 2,
+                image: {
+                    url: 'https://www.bworld.fr/api/file/get/c27e39ee-7ba9-46f8-aa7c-9e334c72a96c/d9d0634b-b1a0-42bd-843d-d3bc3cf7d842/ImageThumb/bworld-2016-v3.png',
+                    title: 'bworld place'
+                }
+            };
+            const app = new App(new Place(new PlaceData())).app;
+            return request(app)
+                .patch('/api/places/999')
+                .send(newPlace)
+                .expect(404);
+        });
+    
+        it('PATCH /api/places/999 should respond a http 404 KO', () => {
+    
+            var newPlace = {
+                name: '',
+                author: 'Pat',
+                review: 2
+            };
+            const app = new App(new Place(new PlaceData())).app;
+            return request(app)
+                .patch('/api/places/999')
+                .send(newPlace)
+                .expect(404);
+        });
+
+            /**************************PATCH REQUESTS + JSONPATCH***********************************/
+    it('PATCH /api/places/2 should respond a http 200 OK with no image', () => {
+        var patch = [
+            { "op": "replace", "path": "/name", "value": "Saint-brieuc" },
+            { "op": "replace", "path": "/author", "value": "Robert" }
+        ]
+        const app = new App(new Place(new PlaceData())).app;
+        return request(app)
+            .patch('/api/places/2')
+            .set({"Content-Type":"application/json-patch+json"})
+            .send(patch)
+            .expect(200)
+            .then(response => {
+                expect(response.body.key).toBe("entity.updated.jsonpatch");
+              });
+    });
+
+    it('PATCH /api/places/2 should respond a http 400 KO', () => {
+
+        var patch = [
+            { "op": "replace", "path": "/name", "value": "Saint-brieuc" },
+            { "op": "replace", "path": "/author", "value": "Robert" }
+        ]
+        const app = new App(new Place(new PlaceData())).app;
+        return request(app)
+            .patch('/api/places/2')
+            .send(patch)
+            .expect(400);
+
+    });
+
+    it('PATCH /api/places/2 should respond a http 400 KO', () => {
+
+        const app = new App(new Place(new PlaceData())).app;
+        var patch = {
+            name: 'Londre',
+            author: 'Patrick',
+            review: 2,
+            image: {
+                url: 'https://www.bworld.fr/api/file/get/c27e39ee-7ba9-46f8-aa7c-9e334c72a96c/d9d0634b-b1a0-42bd-843d-d3bc3cf7d842/ImageThumb/bworld-2016-v3.png',
+                title: 'bworld place'
+            }
+        };
+        return request(app)
+            .patch('/api/places/2')
+            .set({"Content-Type":"application/json-patch+json"})
+            .send(patch)
+            .expect(400);
+            
+    });
+
+    it('PATCH /api/places/999 should respond a http 404 KO with an image', () => {
+
+        var patch = {
+            name: 'Londre',
+            author: 'Patrick',
+            review: 2,
+            image: {
+                url: 'https://www.bworld.fr/api/file/get/c27e39ee-7ba9-46f8-aa7c-9e334c72a96c/d9d0634b-b1a0-42bd-843d-d3bc3cf7d842/ImageThumb/bworld-2016-v3.png',
+                title: 'bworld place'
+            }
+        };
+        const app = new App(new Place(new PlaceData())).app;
+        return request(app)
+            .patch('/api/places/999')
+            .set({"Content-Type":"application/json-patch+json"})
+            .send(patch)
+            .expect(404);
+    });
+
+    it('PATCH /api/places/999 should respond a http 404 KO', () => {
+
+        var patch = [
+            { "op": "replace", "path": "/name", "value": "Saint-brieuc" },
+            { "op": "replace", "path": "/author", "value": "Robert" }
+        ]
+        const app = new App(new Place(new PlaceData())).app;
+        return request(app)
+            .patch('/api/places/999')
+            .set({"Content-Type":"application/json-patch+json"})
+            .send(patch)
+            .expect(404);
+    });
+
+    
 });
